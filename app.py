31771ea1952d715e1c123bc657c6b60d86c9b10b
Matt Ashton <mattashton@google.com>
2025-11-25 22:27:05 +0000

    added two different Google models and separated connections

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+import google.generativeai as genai
+from google.cloud import secretmanager
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+# Create the Secret Manager client.
+client = secretmanager.SecretManagerServiceClient()
+
+# Build the resource name of the secret version.
+name = "projects/396631018769/secrets/optics-app-gemini/versions/latest"
+
+# Access the secret version.
+response = client.access_secret_version(request={"name": name})
+
+# Extract the payload.
+secret_string = response.payload.data.decode("UTF-8")
+
+
+genai.configure(api_key=secret_string)
+model2 = genai.GenerativeModel('gemini-3-pro-preview')
+model1 = genai.GenerativeModel('gemini-2.5-flash-lite')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_news_one')
+def get_news_one():
+    try:
+        prompt = "Tell me a different newsworthy updates in golf world news."
+        response1 = model1.generate_content(prompt)
+        return jsonify({'fact1': response1.text})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/get_news_two')
+def get_news_two():
+    try:
+        prompt = "Tell me a different newsworthy updates in golf world news."
+        response2 = model2.generate_content(prompt)
+        return jsonify({'fact2': response2.text})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
